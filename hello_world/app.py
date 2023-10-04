ef2b1493e5ec43bc039aab155da17e13b3186f30
Joe <joelowrance@gmail.com>
2023-10-04 18:13:51 -0400

    update

---
+++ hello_world/app.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+
+# import requests
+#https://inwpxxtal7.execute-api.us-east-1.amazonaws.com/Stage/hello
+#https://inwpxxtal7.execute-api.us-east-1.amazonaws.com/Prod/hello
+
+
+def lambda_handler(event, context):
+    # to get access to the s3 bucket, I had to
+    # 1. edit the IAM role assigned to the lamda
+    # 2  Gave it full permissions, which I didnt want but dont know how to avoid.
+
+    stage_variable_value = (event['stageVariables'] or {'alias': 'unknown'})["alias"]
+    #stage_variable_value = event['stageVariables']["alias"]
+        #event.get['StageVariables', {}].get('alias', 'wtf')
+
+
+
+
+    s3 = boto3.client('s3')
+    bucket = 'configsjl'
+    response = s3.get_object(Bucket=bucket, Key='simplefile.txt')
+    content = response['Body'].read().decode('utf-8')
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {},
+        # body needs to be converted to json, not just text that looks like json
+        "body": json.dumps({
+            "message": {
+                "content": content,
+                #"event": event,
+                "v1": stage_variable_value
+            }
+        })
+    }
